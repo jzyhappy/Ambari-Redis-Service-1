8f7a5b3132fadc0d9d1b8216d4c8821bc1004f42
jzyhappy <j.zhong.yuan.2009@163.com>
2020-02-24 08:07:19 +0800

    Update params.py
    HDP 2.6.5 ImportError: cannot import name format_hdp_stack_version

--- REDIS-5.0.0/package/scripts/params.py
+++ REDIS-5.0.0/package/scripts/params.py
@@ -4,7 +4,7 @@
 Redis Params configurations
 """
 
-from resource_management.libraries.functions.version import format_hdp_stack_version, compare_versions
+from resource_management.libraries.functions.version import compare_versions
 from resource_management import *
 import status_params
 import os
